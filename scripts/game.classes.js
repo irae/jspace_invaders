3c44e0e50b4db35a88da96a104f424eae9243a95
juliogreff <juliogreff@gmail.com>
2010-01-28 13:17:26 -0200

    refactoring the GamePlayer class

--- scripts/game.classes.js
+++ scripts/game.classes.js
@@ -15,27 +15,42 @@ var GamePlayer = new Class({
     "Extends": GameObject,
     "initialize": function() {
         this.parent("player");
+        
+        // setting animation things
+        this.left = this.object.getCoordinates("stage").left.toInt();
+        this.object.set("morph", {
+            "duration": 50,
+            "link": "chain",
+            "transition": "linear"
+        });
+        
+        // bind the onKeyDown event to this.respondToKey
+        document.addEvent("keydown", this.respondToKey.bind(this));
     },
     "move": function(direction) {
-        var left = this.object.getCoordinates("stage").left.toInt();
-        var nextPosition = left + direction * 34;
-        
-        if(this.canMove(nextPosition)) {
-            this.object.setStyle("left",  nextPosition + "px");
+        var position = this.left + direction * 34;
+        if(this.canMoveTo(position)) {
+            this.left = position;
+            this.object.morph({
+                "left":  this.left + "px"
+            });
         }
     },
-    "canMove": function(position) {
-        console.log(position)
+    "canMoveTo": function(position) {
         return position >= 0 && position <= 642 - 30;
-    }
-});
-
-document.addEvent("keydown", function(e) {
-    var directions = {
-        "left": -1,
-        "right": 1
-    };
-    if(e.key == "right" || e.key == "left") {
-        player.move(directions[e.key]);
+    },
+    "fire": function() {
+        alert("fire!");
+    },
+    "respondToKey": function(e) {
+        switch(e.key) {
+            case "left":
+                return this.move(-1);
+            case "right":
+                return this.move(1);
+            case "space":
+                return this.fire();
+        }
+        return false;
     }
 });
